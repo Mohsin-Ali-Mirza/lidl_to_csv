341bd125f6ca95369b9b449e0c2951a0cfcd4850
Mohsin Mirza <91544857+Mohsin-Ali-Mirza@users.noreply.github.com>
2024-09-28 19:13:34 +0500

    test2

--- app/backend.py
+++ app/backend.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+#app.py
 
-#hello
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -10,6 +10,6 @@ def hello_world():
     return "Hello,World"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app)
